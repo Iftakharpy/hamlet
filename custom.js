1d7b186f9c9875795c970b9f7f4535ba25a00d2e
Iftakhar Ahmed <iftakharn19@gmail.com>
2021-01-04 00:06:48 +0600

    hide arrows, resize slider

--- custom.js
+++ custom.js
@@ -2,5 +2,13 @@
 // wait until the document is ready
 document.addEventListener('DOMContentLoaded', (event) => {
     // resize slider
-    document.querySelector(".slider-banner-3").style = "max-height: 300px; height: 520px;";
+    // document.querySelector(".slider-banner-3").style = "max-height: 500px; height: 520px;";
 });
+
+
+//hide navigation arrows
+$(document).ready(function() {
+    // $("#remove_nav").owlCarousel({
+    //  navigation : false 
+    // });
+  });
